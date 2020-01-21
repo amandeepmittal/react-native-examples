6cae219f983b2a97c99716a0a982d902e8b224b6
Aman Mittal <amandeepmittal@live.com>
2020-01-21 13:27:33 +0530

    add nav in webviews example

---
+++ rnWebViewCustomNav/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useRef } from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  ActivityIndicator,
+  View,
+  TouchableOpacity,
+  Text
+} from 'react-native'
+import WebView from 'react-native-webview'
+
+const App = () => {
+  const [canGoBack, setCanGoBack] = useState(false)
+  const [canGoForward, setCanGoForward] = useState(false)
+  const [currentUrl, setCurrentUrl] = useState('')
+
+  const webviewRef = useRef(null)
+
+  backButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goBack()
+  }
+
+  frontButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goForward()
+  }
+  return (
+    <>
+      <StatusBar barStyle='dark-content' />
+      <SafeAreaView style={styles.flexContainer}>
+        <WebView
+          source={{ uri: 'https://heartbeat.fritz.ai/' }}
+          startInLoadingState={true}
+          renderLoading={() => (
+            <ActivityIndicator
+              color='black'
+              size='large'
+              style={styles.flexContainer}
+            />
+          )}
+          ref={webviewRef}
+          onNavigationStateChange={navState => {
+            setCanGoBack(navState.canGoBack)
+            setCanGoForward(navState.canGoForward)
+            setCurrentUrl(navState.url)
+          }}
+        />
+        <View style={styles.tabBarContainer}>
+          <TouchableOpacity onPress={backButtonHandler}>
+            <Text style={styles.button}>Back</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={frontButtonHandler}>
+            <Text style={styles.button}>Forward</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  flexContainer: {
+    flex: 1
+  },
+  tabBarContainer: {
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#b43757'
+  },
+  button: {
+    color: 'white',
+    fontSize: 24
+  }
+})
+
+export default App
