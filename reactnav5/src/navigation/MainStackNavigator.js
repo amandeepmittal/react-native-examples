3dd990468e25812e8592735a151426673f65ceee
Aman Mittal <amandeepmittal@live.com>
2020-02-08 18:46:32 +0530

    basic stack nav with one screen

---
+++ reactnav5/src/navigation/MainStackNavigator.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Home from '../screens/Home'
+
+const Stack = createStackNavigator()
+
+function MainStackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default MainStackNavigator
