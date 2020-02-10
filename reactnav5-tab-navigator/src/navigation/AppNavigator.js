20ae274253a3e1cfe9daf0f37cb90bee6279e952
Aman Mittal <amandeepmittal@live.com>
2020-02-10 13:47:49 +0530

    Adding icon and changing tint color

--- reactnav5-tab-navigator/src/navigation/AppNavigator.js
+++ reactnav5-tab-navigator/src/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import Home from '../screens/Home'
 import Detail from '../screens/Detail'
@@ -15,10 +16,29 @@ function MainTabNavigator() {
   return (
     <Tab.Navigator
       tabBarOptions={{
-        activeTintColor: '#101010'
+        activeTintColor: '#101010',
+        style: {
+          backgroundColor: '#ffd700'
+        }
       }}>
-      <Tab.Screen name='Home' component={Home} />
-      <Tab.Screen name='Profile' component={Profile} />
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='ios-home' color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='ios-person' size={size} color={color} />
+          )
+        }}
+      />
     </Tab.Navigator>
   )
 }
