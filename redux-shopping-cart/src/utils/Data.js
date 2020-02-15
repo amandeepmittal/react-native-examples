c599d169039f912f02724f86a5ac239cb905dffd
Aman Mittal <amandeepmittal@live.com>
2020-02-15 12:05:25 +0530

    add mock data

---
+++ redux-shopping-cart/src/utils/Data.js
@@ -0,0 +1,51 @@
+export const Books = [
+  {
+    id: 1,
+    name: 'The Book Thief',
+    author: 'Markus Zusak',
+    imgUrl:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1522157426l/19063._SY475_.jpg'
+  },
+  {
+    id: 2,
+    name: 'Sapiens',
+    author: 'Yuval Noah Harari',
+    imgUrl:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1420585954l/23692271.jpg'
+  },
+  {
+    id: 3,
+    name: 'Crime and Punishment',
+    author: 'Fyodor Dostoyevsky',
+    imgUrl:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1382846449l/7144.jpg'
+  },
+  {
+    id: 4,
+    name: 'No Longer Human',
+    author: 'Osamu Dazai',
+    imgUrl:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1422638843l/194746.jpg'
+  },
+  {
+    id: 5,
+    name: 'Atomic Habits',
+    author: 'James Clear',
+    imgUrl:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1535115320l/40121378._SY475_.jpg'
+  },
+  {
+    id: 7,
+    name: 'Dune',
+    author: 'Frank Herbert',
+    imgUrl:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1434908555l/234225._SY475_.jpg'
+  },
+  {
+    id: 8,
+    name: 'Atlas Shrugged',
+    author: 'Ayn Rand',
+    imgUrl:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1405868167l/662.jpg'
+  }
+]
