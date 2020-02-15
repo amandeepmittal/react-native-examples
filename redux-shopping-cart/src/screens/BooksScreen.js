f26d7f3c1ab70089e6a0c2e0ad4e781c1e770c9d
Aman Mittal <amandeepmittal@live.com>
2020-02-15 11:32:53 +0530

    add mock screens

---
+++ redux-shopping-cart/src/screens/BooksScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function BookScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>BookScreen</Text>
+    </View>
+  )
+}
+
+export default BookScreen
