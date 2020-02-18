ea9796b201d26c752aaeaa1471e077b8d581a2a0
Aman Mittal <amandeepmittal@live.com>
2020-02-18 14:44:09 +0530

    change to modal 'mode'

--- reduxTodoApp/src/navigation/AppNavigator.js
+++ reduxTodoApp/src/navigation/AppNavigator.js
@@ -10,7 +10,28 @@ const Stack = createStackNavigator()
 function MainStackNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        mode='modal'
+        headerMode='none'
+        screenOptions={{
+          cardStyle: { backgroundColor: 'transparent' },
+          cardOverlayEnabled: true,
+          cardStyleInterpolator: ({ current: { progress } }) => ({
+            cardStyle: {
+              opacity: progress.interpolate({
+                inputRange: [0, 0.5, 0.9, 1],
+                outputRange: [0, 0.25, 0.7, 1]
+              })
+            },
+            overlayStyle: {
+              opacity: progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 0.5],
+                extrapolate: 'clamp'
+              })
+            }
+          })
+        }}>
         <Stack.Screen name='List' component={ListScreen} />
         <Stack.Screen name='Modal' component={ModalScreen} />
       </Stack.Navigator>
