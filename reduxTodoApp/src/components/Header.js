a6dbcbfaacdd4cbfa00209ef73d8a320446ab968
Aman Mittal <amandeepmittal@live.com>
2020-02-18 16:05:54 +0530

    display items left in list in header

--- reduxTodoApp/src/components/Header.js
+++ reduxTodoApp/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { useSelector } from 'react-redux'
 
 function Header(props) {
   const { title } = props
+  const listItems = useSelector(state => state.itemList)
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{title}</Text>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subTitle}>Left: {listItems.length}</Text>
     </View>
   )
 }
@@ -18,10 +22,15 @@ const styles = StyleSheet.create({
     height: 125,
     paddingTop: 20
   },
-  text: {
+  title: {
     color: '#fff',
     fontSize: 28,
     fontWeight: '500'
+  },
+  subTitle: {
+    paddingTop: 5,
+    fontSize: 18,
+    color: '#fff'
   }
 })
 
