1ae5aa89b657e35d0174822f6099ed953e230e69
Aman Mittal <amandeepmittal@live.com>
2020-02-24 16:15:57 +0530

    change HomeScreen to ChatRooms

--- rnAnonChatApp/src/navigation/StackNavigator.js
+++ rnAnonChatApp/src/navigation/StackNavigator.js
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import HomeScreen from '../screens/HomeScreen'
+import ChatRooms from '../screens/ChatRooms'
 
 const Stack = createStackNavigator()
 
 function HomeStackNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Chat rooms' component={HomeScreen} />
+      <Stack.Screen name='Chat Rooms' component={ChatRooms} />
     </Stack.Navigator>
   )
 }
