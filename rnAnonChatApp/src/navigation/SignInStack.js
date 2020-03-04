26c8f89d22b0a3ba331801aa3c389fc30a6a1a11
Aman Mittal <amandeepmittal@live.com>
2020-03-04 15:45:23 +0530

    add sign in and sign out functionality

--- rnAnonChatApp/src/navigation/SignInStack.js
+++ rnAnonChatApp/src/navigation/SignInStack.js
@@ -1,18 +1,36 @@
 import * as React from 'react'
+import { TouchableOpacity } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import ChatRoom from '../screens/ChatRoom.js'
+import Icon from 'react-native-vector-icons/Ionicons'
+import auth from '@react-native-firebase/auth'
 
 const Stack = createStackNavigator()
 
 export default function SignInStack() {
+  async function logOut() {
+    try {
+      await auth().signOut()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name='ChatRoom'
           component={ChatRoom}
-          options={{ title: 'Chat Room' }}
+          options={{
+            title: 'Chat Room',
+            headerRight: () => (
+              <TouchableOpacity style={{ marginRight: 10 }} onPress={logOut}>
+                <Icon name='ios-log-out' size={30} color='#444' />
+              </TouchableOpacity>
+            )
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- rnAnonChatApp/src/screens/Login.js
+++ rnAnonChatApp/src/screens/Login.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
+import auth from '@react-native-firebase/auth'
 
 export default function Login() {
   // firebase login function later
 
+  async function login() {
+    try {
+      await auth().signInAnonymously()
+    } catch (e) {
+      switch (e.code) {
+        case 'auth/operation-not-allowed':
+          console.log('Enable anonymous in your firebase console.')
+          break
+        default:
+          console.error(e)
+          break
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to 🔥 Chat App</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => alert('Anonymous login')}>
+      <TouchableOpacity style={styles.button} onPress={login}>
         <Text style={styles.buttonText}>Enter Anonymously</Text>
         <Icon name='ios-lock' size={30} color='#cfdce0' />
       </TouchableOpacity>
