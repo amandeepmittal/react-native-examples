c3a4260ba075cda67f13a6d682295e892046267f
Aman Mittal <amandeepmittal@live.com>
2020-03-12 19:21:30 +0530

    Fetch real time messages to display from Firestore collection

--- rnAnonChatApp/src/screens/Messages.js
+++ rnAnonChatApp/src/screens/Messages.js
@@ -1,31 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { GiftedChat } from 'react-native-gifted-chat'
 import firestore from '@react-native-firebase/firestore'
 import auth from '@react-native-firebase/auth'
 
 export default function Messages({ route }) {
-  const [messages, setMessages] = useState([
-    {
-      _id: 0,
-      text: 'thread created',
-      createdAt: new Date().getTime(),
-      system: true
-    },
-    {
-      _id: 1,
-      text: 'hello!',
-      createdAt: new Date().getTime(),
-      user: {
-        _id: 2,
-        name: 'Demo'
-      }
-    }
-  ])
+  const [messages, setMessages] = useState([])
+  const { thread } = route.params
+  const user = auth().currentUser.toJSON()
 
-  async function handleSend(newMessage = []) {
+  // fetch messages from Firestore
+  useEffect(() => {
+    const unsubscribeListener = firestore()
+      .collection('MESSAGE_THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(querySnapshot => {
+        const messages = querySnapshot.docs.map(doc => {
+          const firebaseData = doc.data()
+
+          const data = {
+            _id: doc.id,
+            text: '',
+            createdAt: new Date().getTime(),
+            ...firebaseData
+          }
+
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.displayName
+            }
+          }
+
+          return data
+        })
+
+        // console.log(messages)
+        setMessages(messages)
+      })
+
+    return () => unsubscribeListener() // Stop listening for updates whenever the component unmounts
+  }, [])
+
+  async function handleSend(messages) {
     const text = messages[0].text
-    const { thread } = route.params
-    const user = auth().currentUser.toJSON()
 
     await firestore()
       .collection('MESSAGE_THREADS')
@@ -48,19 +67,18 @@ export default function Messages({ route }) {
         text,
         createdAt: new Date().getTime(),
         user: {
-          _id: user.uid
+          _id: user.uid,
+          displayName: user.displayName
         }
       })
-
-    setMessages(GiftedChat.append(messages, newMessage))
   }
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={newMessage => handleSend(newMessage)}
+      onSend={handleSend}
       user={{
-        _id: 1
+        _id: user.uid
       }}
     />
   )
