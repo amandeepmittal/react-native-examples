fc9ea137f6625fe9085f0ddc51918fb5c9a93e46
Aman Mittal <amandeepmittal@live.com>
2020-03-19 11:08:29 +0530

    set up tab navigator with two screens

--- customStatusBarHook/App.js
+++ customStatusBarHook/App.js
@@ -1,19 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AppTabs from "./src/navigation/AppTabs";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <AppTabs />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ customStatusBarHook/src/navigation/AppTabs.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20, color: "#333333" }}>Home Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#be79df"
+      }}
+    >
+      <Text style={{ fontSize: 20, color: "white" }}>Settings Screen</Text>
+    </View>
+  );
+}
+
+const Tabs = createBottomTabNavigator();
+
+export default function AppTabs() {
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-list-box" : "ios-list";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray"
+        }}
+      >
+        <Tabs.Screen name="Home" component={HomeScreen} />
+        <Tabs.Screen name="Settings" component={SettingsScreen} />
+      </Tabs.Navigator>
+    </NavigationContainer>
+  );
+}
