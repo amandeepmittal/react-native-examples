c7317037ce7bb53951c6ab176cbc590dbecc7aa6
Aman Mittal <amandeepmittal@live.com>
2020-03-21 11:19:17 +0530

    install react-navigation

--- rnChatApp/index.js
+++ rnChatApp/index.js
@@ -1,3 +1,5 @@
+// import for prod from 'react-navigation' integration
+import "react-native-gesture-handler";
 import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
