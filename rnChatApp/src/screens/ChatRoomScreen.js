6ce38d9485aa2592c9a4eb6402b7611a44e7a210
Aman Mittal <amandeepmittal@live.com>
2020-03-21 11:24:01 +0530

    create mockup screens

---
+++ rnChatApp/src/screens/ChatRoomScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ChatRoomScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Chat Rooms</Text>
+    </View>
+  );
+}
