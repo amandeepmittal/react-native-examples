c2714b79881f5461a9c0e06eae4ace6afcd58cbc
Aman Mittal <amandeepmittal@live.com>
2020-03-23 17:25:13 +0530

    remove

--- authFlow/src/navigation/AuthNavigator.js
+++ authFlow/src/navigation/AuthNavigator.js
@@ -15,12 +15,12 @@ export default function AuthNavigator() {
     if (initializing) setInitializing(false)
   }
 
-  useEffect(() => {
-    const authSubscriber = auth().onAuthStateChanged(onAuthStateChanged)
+  // useEffect(() => {
+  //   const authSubscriber = auth().onAuthStateChanged(onAuthStateChanged)
 
-    // unsubscribe on unmount
-    return authSubscriber
-  }, [])
+  //   // unsubscribe on unmount
+  //   return authSubscriber
+  // }, [])
 
   if (initializing) {
     return null
@@ -33,4 +33,61 @@ export default function AuthNavigator() {
   ) : (
     <SignOutStack />
   )
+}
+
+class Initial extends Component {
+  state = {
+    isAssetsLoadingComplete: false
+  }
+
+  componentDidMount = async () => {
+    try {
+      // previously
+      this.loadLocalAsync()
+
+      await this.props.firebase.checkUserAuth(user => {
+        if (user) {
+          // if the user has previously logged in
+          this.props.navigation.navigate('App')
+        } else {
+          // if the user has previously signed out from the app
+          this.props.navigation.navigate('Auth')
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  loadLocalAsync = async () => {
+    return await Promise.all([
+      Asset.loadAsync([
+        require('../assets/flame.png'),
+        require('../assets/icon.png')
+      ]),
+      Font.loadAsync({
+        ...Icon.Ionicons.font
+      })
+    ])
+  }
+
+  handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error)
+  }
+
+  handleFinishLoading = () => {
+    this.setState({ isAssetsLoadingComplete: true })
+  }
+
+  render() {
+    return (
+      <AppLoading
+        startAsync={this.loadLocalAsync}
+        onFinish={this.handleFinishLoading}
+        onError={this.handleLoadingError}
+      />
+    )
+  }
 }
--- rnChatApp/index.js
+++
@@ -1,7 +0,0 @@
-// import for prod from 'react-navigation' integration
-import "react-native-gesture-handler";
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent(appName, () => App);
