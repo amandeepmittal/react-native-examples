e412d67b1823dc5206c89f75c44154d575790543
Aman Mittal <amandeepmittal@live.com>
2020-03-24 15:15:35 +0530

    refactor

--- ChatApp/src/navigation/AuthStack.js
+++ ChatApp/src/navigation/AuthStack.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import SignupScreen from '../screens/SignupScreen';
 import LoginScreen from '../screens/LoginScreen';
 
