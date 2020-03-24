ead9d0d48c9c7045c481b2fd70240285bcbb790e
Aman Mittal <amandeepmittal@live.com>
2020-03-24 14:43:27 +0530

    sign up screen

---
+++ ChatApp/src/screens/SignupScreen.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Title } from 'react-native-paper';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+
+export default function SignupScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Title style={styles.titleText}>Register to chat</Title>
+      <FormInput
+        labelName='Email'
+        value={email}
+        autoCapitalize='none'
+        onChangeText={userEmail => setEmail(userEmail)}
+      />
+      <FormInput
+        labelName='Password'
+        value={password}
+        secureTextEntry={true}
+        onChangeText={userPassword => setPassword(userPassword)}
+      />
+      <FormButton
+        title='Signup'
+        modeValue='contained'
+        labelStyle={styles.loginButtonLabel}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleText: {
+    fontSize: 24,
+    marginBottom: 10
+  },
+  loginButtonLabel: {
+    fontSize: 22
+  },
+  navButtonText: {
+    fontSize: 16
+  }
+});
