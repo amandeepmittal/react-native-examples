1b786a496017261951798b2f7f9cf2ab303e4221
Aman Mittal <amandeepmittal@live.com>
2020-03-26 21:13:18 +0530

    complete modal screen & add firestore collection THREADS

--- ChatApp/src/screens/AddRoomScreen.js
+++ ChatApp/src/screens/AddRoomScreen.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { IconButton } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { IconButton, Title } from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
 
 export default function AddRoomScreen({ navigation }) {
+  const [roomName, setRoomName] = useState('');
+
+  /**
+   * Create a new Firestore collection to save threads
+   */
+  function handleButtonPress() {
+    if (roomName.length > 0) {
+      firestore()
+        .collection('THREADS')
+        .add({
+          name: roomName,
+          latestMessage: {
+            text: `${roomName} created.`,
+            createdAt: new Date().getTime()
+          }
+        })
+        .then(() => {
+          navigation.navigate('Home');
+        });
+    }
+  }
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ position: 'absolute', top: 30, right: 0, zIndex: 1 }}>
+    <View style={styles.rootContainer}>
+      <View style={styles.closeButtonContainer}>
         <IconButton
           icon='close-circle'
           size={36}
@@ -13,9 +37,45 @@ export default function AddRoomScreen({ navigation }) {
           onPress={() => navigation.goBack()}
         />
       </View>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Create a new chat room</Text>
+      <View style={styles.innerContainer}>
+        <Title style={styles.title}>Create a new chat room</Title>
+        <FormInput
+          labelName='Room Name'
+          value={roomName}
+          onChangeText={text => setRoomName(text)}
+        />
+        <FormButton
+          title='Create'
+          modeValue='contained'
+          labelStyle={styles.buttonLabel}
+          onPress={() => handleButtonPress()}
+          disabled={roomName.length === 0}
+        />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1
+  },
+  closeButtonContainer: {
+    position: 'absolute',
+    top: 30,
+    right: 0,
+    zIndex: 1
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10
+  },
+  buttonLabel: {
+    fontSize: 22
+  }
+});
