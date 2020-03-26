a8995fd45c26d09e497e10eb3b82c8e123a082dd
Aman Mittal <amandeepmittal@live.com>
2020-03-26 20:26:22 +0530

    add close button to modal screen with position absolute

--- ChatApp/src/screens/AddRoomScreen.js
+++ ChatApp/src/screens/AddRoomScreen.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import FormButton from '../components/FormButton';
+import { IconButton } from 'react-native-paper';
 
 export default function AddRoomScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Create a new chat room</Text>
-      <FormButton
-        mode='contained'
-        title='Close Modal'
-        onPress={() => navigation.goBack()}
-      />
+    <View style={{ flex: 1 }}>
+      <View style={{ position: 'absolute', top: 30, right: 0, zIndex: 1 }}>
+        <IconButton
+          icon='close-circle'
+          size={36}
+          color='#6646ee'
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Create a new chat room</Text>
+      </View>
     </View>
   );
 }
