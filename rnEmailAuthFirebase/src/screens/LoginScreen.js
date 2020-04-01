c4516db689abf7136555bbc6d3454be524e26ba8
Aman Mittal <amandeepmittal@live.com>
2020-04-01 12:26:30 +0530

    create auth screens

---
+++ rnEmailAuthFirebase/src/screens/LoginScreen.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import FormButton from '../components/FormButton';
+import FormInput from '../components/FormInput';
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Firebase app</Text>
+      <FormInput
+        value={email}
+        placeholderText='Email'
+        onChangeText={userEmail => setEmail(userEmail)}
+        autoCapitalize='none'
+        keyboardType='email-address'
+        autoCorrect={false}
+      />
+      <FormInput
+        value={password}
+        placeholderText='Password'
+        onChangeText={userPassword => setPassword(userPassword)}
+        secureTextEntry={true}
+      />
+      <FormButton buttonTitle='Login' onPress={() => alert('login button')} />
+      <TouchableOpacity
+        style={styles.navButton}
+        onPress={() => navigation.navigate('Signup')}
+      >
+        <Text style={styles.navButtonText}>New user? Join here</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 10
+  },
+  navButton: {
+    marginTop: 15
+  },
+  navButtonText: {
+    fontSize: 20,
+    color: '#6646ee'
+  }
+});
---
+++ rnEmailAuthFirebase/src/screens/SignupScreen.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FormButton from '../components/FormButton';
+import FormInput from '../components/FormInput';
+
+export default function SignupScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Create an account</Text>
+      <FormInput
+        value={email}
+        placeholderText='Email'
+        onChangeText={userEmail => setEmail(userEmail)}
+        autoCapitalize='none'
+        keyboardType='email-address'
+        autoCorrect={false}
+      />
+      <FormInput
+        value={password}
+        placeholderText='Password'
+        onChangeText={userPassword => setPassword(userPassword)}
+        secureTextEntry={true}
+      />
+      <FormButton buttonTitle='Signup' onPress={() => alert('sign button')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 10
+  }
+});
