26226996bd6ad0b26750b9c9cccb0bb9aabd987c
Aman Mittal <amandeepmittal@live.com>
2020-04-01 13:56:24 +0530

    create routes

---
+++ rnEmailAuthFirebase/src/navigation/Routes.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
+import AuthStack from './AuthStack';
+import HomeStack from './HomeStack';
+import { AuthContext } from './AuthProvider';
+import Loading from '../components/Loading';
+
+export default function Routes() {
+  const { user, setUser } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const [initializing, setInitializing] = useState(true);
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <NavigationContainer>
+      {user ? <HomeStack /> : <AuthStack />}
+    </NavigationContainer>
+  );
+}
