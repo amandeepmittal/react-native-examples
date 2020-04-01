4bd6a0f5be65213244ef818fefe6e188941b3203
Aman Mittal <amandeepmittal@live.com>
2020-04-01 12:25:59 +0530

    create reusable components

---
+++ rnEmailAuthFirebase/src/components/FormButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { windowHeight, windowWidth } from '../utils/Dimensions';
+
+export default function FormButton({ buttonTitle, ...rest }) {
+  return (
+    <TouchableOpacity style={styles.buttonContainer} {...rest}>
+      <Text style={styles.buttonText}>{buttonTitle}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 10,
+    width: windowWidth / 2,
+    height: windowHeight / 15,
+    backgroundColor: '#6646ee',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8
+  },
+  buttonText: {
+    fontSize: 28,
+    color: '#ffffff'
+  }
+});
---
+++ rnEmailAuthFirebase/src/components/FormInput.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+import { windowHeight, windowWidth } from '../utils/Dimensions';
+
+export default function FormInput({ labelValue, placeholderText, ...rest }) {
+  return (
+    <TextInput
+      value={labelValue}
+      style={styles.input}
+      numberOfLines={1}
+      placeholder={placeholderText}
+      placeholderTextColor='#666'
+      {...rest}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 10,
+    width: windowWidth / 1.5,
+    height: windowHeight / 15,
+    fontSize: 16,
+    borderRadius: 8,
+    borderWidth: 1
+  }
+});
