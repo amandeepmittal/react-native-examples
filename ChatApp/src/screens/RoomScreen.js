8c720ca96a99e89422408fab7352f7ad53d9c1eb
Aman Mittal <amandeepmittal@live.com>
2020-04-06 01:37:53 +0530

    checkout gifted chat props and options

--- ChatApp/src/screens/RoomScreen.js
+++ ChatApp/src/screens/RoomScreen.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, Send } from 'react-native-gifted-chat';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
+import { IconButton } from 'react-native-paper';
 
 export default function RoomScreen() {
   const [messages, setMessages] = useState([
@@ -24,17 +26,85 @@ export default function RoomScreen() {
       }
     }
   ]);
-
   // helper method that is sends a message
   function handleSend(newMessage = []) {
     setMessages(GiftedChat.append(messages, newMessage));
+  }
+
+  function renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#6646ee'
+          }
+        }}
+        textStyle={{
+          right: {
+            color: '#fff'
+          }
+        }}
+      />
+    );
+  }
+
+  function renderLoading() {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color='#6646ee' />
+      </View>
+    );
+  }
+
+  function renderSend(props) {
+    return (
+      <Send {...props}>
+        <View style={styles.sendingContainer}>
+          <IconButton icon='send-circle' size={32} color='#6646ee' />
+        </View>
+      </Send>
+    );
+  }
+
+  function scrollToBottomComponent() {
+    return (
+      <View style={styles.bottomComponentContainer}>
+        <IconButton icon='chevron-double-down' size={36} color='#6646ee' />
+      </View>
+    );
   }
 
   return (
     <GiftedChat
       messages={messages}
       onSend={newMessage => handleSend(newMessage)}
-      user={{ _id: 1 }}
+      user={{ _id: 1, name: 'User Test' }}
+      placeholder='What do you have to say?'
+      alwaysShowSend
+      showUserAvatar
+      scrollToBottom
+      renderBubble={renderBubble}
+      renderLoading={renderLoading}
+      renderSend={renderSend}
+      scrollToBottomComponent={scrollToBottomComponent}
+      isTyping={true}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sendingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottomComponentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
