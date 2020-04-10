968eec29df95e79ea0aab70151948aca6751aca3
Aman Mittal <amandeepmittal@live.com>
2020-04-10 14:47:15 +0530

    refactor

--- ChatApp/src/screens/RoomScreen.js
+++ ChatApp/src/screens/RoomScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { GiftedChat, Bubble, Send } from 'react-native-gifted-chat';
 import { ActivityIndicator, View, StyleSheet } from 'react-native';
 import { IconButton } from 'react-native-paper';
-import { AuthContext } from '../navigation/AuthProvider';
+// import { AuthContext } from '../navigation/AuthProvider';
 
 export default function RoomScreen() {
   const [messages, setMessages] = useState([
@@ -14,7 +14,7 @@ export default function RoomScreen() {
       _id: 0,
       text: 'New room created.',
       createdAt: new Date().getTime(),
-      system: true,
+      system: true
     },
     // example of chat message
     {
@@ -23,13 +23,13 @@ export default function RoomScreen() {
       createdAt: new Date().getTime(),
       user: {
         _id: 2,
-        name: 'Test User',
-      },
-    },
+        name: 'Test User'
+      }
+    }
   ]);
 
-  const { user } = useContext(AuthContext);
-  const currentUser = user.toJSON();
+  // const { user } = useContext(AuthContext);
+  // const currentUser = user.toJSON();
 
   // helper method that is sends a message
   async function handleSend(newMessage = []) {
@@ -42,13 +42,13 @@ export default function RoomScreen() {
         {...props}
         wrapperStyle={{
           right: {
-            backgroundColor: '#6646ee',
-          },
+            backgroundColor: '#6646ee'
+          }
         }}
         textStyle={{
           right: {
-            color: '#fff',
-          },
+            color: '#fff'
+          }
         }}
       />
     );
@@ -83,7 +83,7 @@ export default function RoomScreen() {
   return (
     <GiftedChat
       messages={messages}
-      onSend={(newMessage) => handleSend(newMessage)}
+      onSend={newMessage => handleSend(newMessage)}
       user={{ _id: 1, name: 'User Test' }}
       placeholder='Type your message here...'
       alwaysShowSend
@@ -93,7 +93,6 @@ export default function RoomScreen() {
       renderLoading={renderLoading}
       renderSend={renderSend}
       scrollToBottomComponent={scrollToBottomComponent}
-      isTyping={true}
     />
   );
 }
@@ -102,14 +101,14 @@ const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   sendingContainer: {
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   bottomComponentContainer: {
     justifyContent: 'center',
-    alignItems: 'center',
-  },
+    alignItems: 'center'
+  }
 });
