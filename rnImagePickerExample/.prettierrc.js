59c7149077e69d9d9b338b682dbae5a28768a84f
Aman Mittal <amandeepmittal@live.com>
2020-04-20 16:18:09 +0530

    initial commit

---
+++ rnImagePickerExample/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  jsxBracketSameLine: false,
+};
