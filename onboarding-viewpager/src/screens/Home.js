fa2c0a1f58427863283d0775469bb8caf4330061
Aman Mittal <amandeepmittal@live.com>
2020-08-26 18:53:14 +0530

    add rnav lib

---
+++ onboarding-viewpager/src/screens/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eb8f8f'
+  },
+  text: {
+    fontSize: 20
+  }
+});
+
+export default Home;
