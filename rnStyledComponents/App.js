a9a4ba1042afbb6910d40ca7e4ebe796d807adea
amandeepmittal <amandeepmittal@live.com>
2020-09-26 19:00:01 +0530

    add header

--- rnStyledComponents/App.js
+++ rnStyledComponents/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import PressableButton from './components/PressabeButton';
+import Header from './components/Header';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
   flex: 1;
   background-color: white;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Text = styled.Text`
-  font-size: 18px;
-  color: blue;
-  font-weight: 500;
 `;
 
 export default function App() {
   return (
     <Container>
-      <Text>Open up App.js to start working on your app!</Text>
-      <PressableButton
-        onPress={() => true}
-        title='First button'
-        bgColor='papayawhip'
-      />
-      <PressableButton
-        onPress={() => true}
-        title='First button'
-        bgColor='#4267B2'
-        primary
-      />
+      <Header headerTitle='social' />
     </Container>
   );
 }
---
+++ rnStyledComponents/components/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Container = styled.View`
+  width: 100%;
+  height: 50px;
+  padding-horizontal: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderTitle = styled.Text`
+  font-size: 26px;
+  font-weight: 700;
+  letter-spacing: 0.25px;
+  color: #3a86e9;
+`;
+
+const IconButtonsRow = styled.View`
+  flex-direction: row;
+`;
+
+const IconButton = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background: #e6e6e6;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
+`;
+
+const Header = ({ headerTitle }) => {
+  return (
+    <Container>
+      <HeaderTitle>{headerTitle}</HeaderTitle>
+      <IconButtonsRow>
+        <IconButton activeOpacity={0.7} onPress={() => true}>
+          <MaterialCommunityIcons name='magnify' size={28} color='#010101' />
+        </IconButton>
+        <IconButton activeOpacity={0.7} onPress={() => true}>
+          <MaterialCommunityIcons
+            name='facebook-messenger'
+            size={28}
+            color='#010101'
+          />
+        </IconButton>
+      </IconButtonsRow>
+    </Container>
+  );
+};
+
+export default Header;
