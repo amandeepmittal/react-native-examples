6deb685e0880a3dd3b0f17046fa923f7bf511bb7
amandeepmittal <amandeepmittal@live.com>
2020-11-25 22:46:20 +0530

    Create a React Native app with expo-cli

--- redux-persist-asyncstorage/screens/BooksList.js
+++ redux-persist-asyncstorage/screens/BooksList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function BooksListApp() {
+export default function BooksList() {
   return (
     <View style={styles.container}>
       <Text>BooksList</Text>
