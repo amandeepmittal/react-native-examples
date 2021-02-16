7758329b64e4d34300eb0c9eb0cd151c0a6b1130
amandeepmittal <amandeepmittal@live.com>
2021-02-16 20:14:44 +0530

    done

--- expo-geolocation-example/screens/Home.js
+++ expo-geolocation-example/screens/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Home = () => {
+const Home = ({ route }) => {
+  const { item } = route.params;
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>Home Delivery address: </Text>
+        <Text style={styles.text}>{item}</Text>
+      </View>
     </View>
   );
 };
@@ -15,6 +19,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#070707',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  contentContainer: {
+    paddingHorizontal: 20,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: '700',
+    color: '#FD0139',
+    paddingBottom: 10
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '400',
+    color: '#fff'
   }
 });
 
--- expo-geolocation-example/screens/Welcome.js
+++ expo-geolocation-example/screens/Welcome.js
@@ -1,14 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
+import * as Location from 'expo-location';
 
-const Welcome = () => {
+const Welcome = ({ navigation }) => {
+  const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
+
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
+    'Wait, we are fetching you location...'
+  );
+
+  useEffect(() => {
+    CheckIfLocationEnabled();
+    GetCurrentLocation();
+  }, []);
+
+  const CheckIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+
+    if (!enabled) {
+      Alert.alert(
+        'Location Service not enabled',
+        'Please enable your location services to continue',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    } else {
+      setLocationServiceEnabled(enabled);
+    }
+  };
+
+  const GetCurrentLocation = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+      Alert.alert(
+        'Permission not granted',
+        'Allow the app to use location service.',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    }
+
+    let { coords } = await Location.getCurrentPositionAsync();
+
+    if (coords) {
+      const { latitude, longitude } = coords;
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude
+      });
+
+      for (let item of response) {
+        // console.log(item)
+        let address = `${item.name}, ${item.street}, ${item.postalCode}, ${item.city}`;
+
+        setDisplayCurrentAddress(address);
+
+        if (address.length > 0) {
+          setTimeout(() => {
+            navigation.navigate('Home', { item: address });
+          }, 2000);
+        }
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
         <Image source={require('../assets/geo.png')} style={styles.image} />
         <Text style={styles.title}>What's your address?</Text>
       </View>
-      <Text style={styles.text}>Mock Address</Text>
+      <Text style={styles.text}>{displayCurrentAddress}</Text>
     </View>
   );
 };
