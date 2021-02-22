9edec825c9c73447e61d1193ae483a47e1664513
amandeepmittal <amandeepmittal@live.com>
2021-02-22 14:02:03 +0530

    :white_check_mark:  SVG imports working

--- expo-prototype-template/App.js
+++ expo-prototype-template/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Twitter, Instagram } from './assets/images';
+
 export default function App() {
   return (
     <>
       <StatusBar style='auto' />
       <View style={styles.container}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            padding: 10
+          }}
+        >
+          <Twitter height={100} width={100} />
+          <Instagram height={100} width={100} />
+        </View>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     </>
