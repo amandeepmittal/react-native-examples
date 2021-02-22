7b7641c83df513a636443b48606922e27107d0b7
amandeepmittal <amandeepmittal@live.com>
2021-02-22 16:38:55 +0530

    Update App.js

--- expo-prototype-template/App.js
+++ expo-prototype-template/App.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
 import AppLoading from 'expo-app-loading';
 
 import loadAssetAsync from './src/constants/CacheAssets';
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
-  const [assetsLoading, setAssetsLoading] = useState(false);
+  const [assetsLoading, setAssetsLoading] = useState(true);
 
   if (assetsLoading) {
     return (
       <AppLoading
         startAsync={loadAssetAsync}
-        onFinish={setAssetsLoading(true)}
+        onFinish={setAssetsLoading(false)}
         onError={() => console.warn}
       />
     );
@@ -26,12 +25,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
