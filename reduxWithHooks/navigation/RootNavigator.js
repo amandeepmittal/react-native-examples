24659a8220db8237888b7989ed55fb1d41cb05a7
amandeepmittal <amandeepmittal@live.com>
2021-03-28 21:12:40 +0530

    customize tab bar

--- reduxWithHooks/navigation/RootNavigator.js
+++ reduxWithHooks/navigation/RootNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 // import screens
 import Movies from '../screens/Movies';
@@ -8,12 +9,38 @@ import Favorites from '../screens/Favorites';
 
 const Tab = createBottomTabNavigator();
 
+const tabBarOptions = {
+  showLabel: false,
+  inactiveTintColor: '#2D3038',
+  activeTintColor: '#FFFFFF',
+  style: {
+    height: '10%',
+    backgroundColor: '#1E1B26',
+  },
+};
+
 const RootNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Movies" component={Movies} />
-        <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Navigator tabBarOptions={tabBarOptions}>
+        <Tab.Screen
+          name="Movies"
+          component={Movies}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialIcons name="movie-filter" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={Favorites}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialIcons name="favorite" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
