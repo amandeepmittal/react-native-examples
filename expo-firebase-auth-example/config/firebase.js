673e716c39fa4d7cfc8cc29e8d1a68879e207ca3
amandeepmittal <amandeepmittal@live.com>
2021-06-11 18:21:41 +0530

    ## Using Environment variables

---
+++ expo-firebase-auth-example/config/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import Constants from 'expo-constants';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId
+};
+
+let Firebase;
+
+if (firebase.apps.length === 0) {
+  Firebase = firebase.initializeApp(firebaseConfig);
+}
+
+export default Firebase;
