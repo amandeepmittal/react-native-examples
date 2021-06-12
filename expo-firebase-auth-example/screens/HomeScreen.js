3f7e7feb90dbdc01edfc400ce9c7a0d7b84e4598
amandeepmittal <amandeepmittal@live.com>
2021-06-12 18:21:48 +0530

    done

--- expo-firebase-auth-example/screens/HomeScreen.js
+++ expo-firebase-auth-example/screens/HomeScreen.js
@@ -1,18 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { IconButton } from '../components';
+import Firebase from '../config/firebase';
+import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
+
+const auth = Firebase.auth();
 
 export default function HomeScreen() {
+  const { user } = useContext(AuthenticatedUserContext);
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View style={styles.container}>
       <StatusBar style='dark-content' />
       <View style={styles.row}>
-        <Text style={styles.title}>Welcome, User</Text>
-        <IconButton name='logout' size={24} color='#fff' onPress={() => true} />
+        <Text style={styles.title}>Welcome {user.email}!</Text>
+        <IconButton
+          name='logout'
+          size={24}
+          color='#fff'
+          onPress={handleSignOut}
+        />
       </View>
-      <Text style={styles.text}>Your UID is: </Text>
+      <Text style={styles.text}>Your UID is: {user.uid} </Text>
     </View>
   );
 }
