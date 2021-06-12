76e0b48b096582f47247062ad5c871cc4de69b88
amandeepmittal <amandeepmittal@live.com>
2021-06-12 17:48:03 +0530

    complete  navigation and create auth context

---
+++ expo-firebase-auth-example/navigation/AuthStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+
+const Stack = createStackNavigator();
+
+export default function AuthStack() {
+  return (
+    <Stack.Navigator headerMode='none'>
+      <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Screen name='Signup' component={SignupScreen} />
+    </Stack.Navigator>
+  );
+}
