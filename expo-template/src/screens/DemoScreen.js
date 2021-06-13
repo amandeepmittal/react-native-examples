f353210a86f7fdcdc4efa0966f9770f98046b481
amandeepmittal <amandeepmittal@live.com>
2021-06-13 16:20:29 +0530

    Add button Demo

--- expo-template/src/screens/DemoScreen.js
+++ expo-template/src/screens/DemoScreen.js
@@ -1,23 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 
 import { theme } from '../constants';
+import { Button } from '../components';
 
 export default function DemoScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Demo Screen Placeholder</Text>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Text style={styles.text}>Demo Screen Placeholder</Text>
+        <Button
+          title='Solid'
+          backgroundColor={theme.primary}
+          containerStyle={styles.button}
+        />
+        <Button
+          title='Outline'
+          outline
+          titleColor={theme.strong}
+          containerStyle={styles.button}
+          width='75%'
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: theme.background,
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: theme.background
+  },
+  scrollContainer: {
+    paddingHorizontal: 16
+  },
+  text: {
+    fontSize: 22,
+    color: theme.primary,
+    fontWeight: '600'
+  },
+  button: {
+    marginTop: 20
   }
 });
