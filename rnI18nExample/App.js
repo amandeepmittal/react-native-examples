cfba03448997ed23bd55c2766d2e0bf172d89b85
amandeepmittal <amandeepmittal@live.com>
2021-08-03 17:24:55 +0530

    delete this

---
+++ rnI18nExample/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import RootNavigator from './src/navigation/RootNavigator';
+
+export default function App() {
+  return <RootNavigator />;
+}
---
+++ rnI18nExample/src/screens/SettingsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
