9f2e2018ae129eb598db6d10852248e1f55749f3
Aman Mittal <amandeepmittal@live.com>
2021-09-04 19:02:19 +0530

    initial commit

---
+++ rnSplashAndIconExample/android/app/src/main/java/com/rnsplashandiconexample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.rnsplashandiconexample;
+
+import com.facebook.react.ReactActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "rnSplashAndIconExample";
+  }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+}
