f1dcd0b3f4d58eaf6c9b5785e076c26bff8fbc99
Aman Mittal <amandeepmittal@live.com>
2021-12-16 16:30:33 +0530

    update

--- tamagui-example/App.js
+++ tamagui-example/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -16,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
