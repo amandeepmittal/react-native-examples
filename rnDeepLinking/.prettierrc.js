60d5fb7f8a3ddf666c665468f1d786ff2de77f9a
Aman Mittal <amandeepmittal@live.com>
2022-03-13 13:09:03 +0530

    update prettier config

--- rnDeepLinking/.prettierrc.js
+++ rnDeepLinking/.prettierrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   singleQuote: true,
-  jsxSingleQuote: true,
   trailingComma: 'none',
   arrowParens: 'avoid',
   proseWrap: 'preserve',
   quoteProps: 'as-needed',
   jsBracketSameLine: false,
-  bracketSpacing: true  
+  bracketSpacing: true
 };
