72fa2b226a0e921390ed99f08f7fbe47206685f5
Aman Mittal <amandeepmittal@live.com>
2022-03-13 13:04:14 +0530

    create screens and nav files

---
+++ rnDeepLinking/src/screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DetailsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export default DetailsScreen;
---
+++ rnDeepLinking/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
