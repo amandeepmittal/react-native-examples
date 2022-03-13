09ae928253a2306809b76f82b5e5640ea77882c1
Aman Mittal <amandeepmittal@live.com>
2022-03-13 12:58:30 +0530

    initial commit: set up react native app

---
+++ rnDeepLinking/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  jsBracketSameLine: false,
+  bracketSpacing: true  
+};
---
+++ rnDeepLinking/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Deep Linking Example</Text>
+    </View>
+  );
+};
+
+export default App;
