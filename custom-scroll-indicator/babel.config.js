2a33c41f16eb0fe6109f0cfa18207232397e0e2e
Aman Mittal <amandeepmittal@live.com>
2022-05-02 18:15:07 +0530

    custom-scroll-indicator: upgrade expo sdk to 44

--- custom-scroll-indicator/babel.config.js
+++ custom-scroll-indicator/babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['@babel/plugin-proposal-unicode-property-regex'],
   };
 };
