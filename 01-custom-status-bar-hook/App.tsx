17d34cf02da7e7a9e73603704b1a29ed192ba0af
Aman Mittal <amandeepmittal@live.com>
2022-12-01 18:12:02 +0530

    Convert to TypeScript

--- 01-custom-status-bar-hook/App.tsx
+++ 01-custom-status-bar-hook/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
--- 01-custom-status-bar-hook/src/navigation/RootNavigator.tsx
+++ 01-custom-status-bar-hook/src/navigation/RootNavigator.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
